21808b24d932d3172776a0fb3091b06ae914f35a
gaglage <gabialvarez@gmail.com>
2018-12-08 10:02:52 +0100

    item logic

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,16 +1,38 @@
 <template>
-    <div>
-
-    </div>
+  <div></div>
 </template>
 
 <script>
-import frio
-    export default {
-
+import { mapMutations, mapActions } from "vuex";
+export default {
+  props: {
+    todo: {
+      type: Object,
+      required: true
+    },
+    methods: {
+      ...mapActions({
+        _updateTodoStatus: "todos/updateTodoStatus",
+        _removeTodo: "todos/removeTodo"
+      }),
+      ...mapMutations("todos", ["setTodo"]),
+      getUpdateTodo(todo) {
+        this.setTodo(todo);
+        this.$router.push({
+          name: "todos-update",
+          params: { id: todo.id }
+        });
+      },
+      updateTodoStatus(todo){
+          this._updateTodoStatus(todo)
+      },
+      removeTodo(id){
+          this._removeTodo(todo)
+      }
     }
+  }
+};
 </script>
 
 <style scoped>
-
 </style>
